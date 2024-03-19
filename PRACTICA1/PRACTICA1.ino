1975966c39c5e0ca225f04da18c75135bc0094f6
diegoacondori <diego.condori@ucb.edu.bo>
2024-03-18 22:56:46 -0400

    Blink

--- PRACTICA1/PRACTICA1.ino
+++ PRACTICA1/PRACTICA1.ino
@@ -1,25 +1,54 @@
-#define LED1 1 
-#define LED2 2
-#define LED3 3
-#define tiempo 1000
+//ELAVORADO POR DIEGO A CONDORI Y RICARDO VELASCO LOPEZ
+#define LED1 5 
+#define LED2 6
+#define LED3 7
+int pul=8;
+int valpul;
+int dtled=200;
+int cont=0;
 
 void setup() {
- pinMode (LED1,OUTPUT);
- pinMode (LED2,OUTPUT);
- pinMode (LED3,OUTPUT);
+  // put your setup code here, to run once:
+  pinMode(pul,INPUT_PULLUP);
+  pinMode (LED1,OUTPUT);
+  pinMode (LED2,OUTPUT);
+  pinMode (LED3,OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
+ // put your main code here, to run repeatedly:
+valpul=digitalRead(pul);
+Serial.println(100);
+delay(100);
+
+ if(valpul==1){
   digitalWrite(LED1,HIGH);
-  delay(tiempo);
+  delay(dtled);
   digitalWrite(LED1,LOW);
-  delay(tiempo);
+  delay(dtled);
   digitalWrite(LED2,HIGH);
-  delay(tiempo);
+  delay(dtled);
   digitalWrite(LED2,LOW);
-  delay(tiempo);
+  delay(dtled);
   digitalWrite(LED3,HIGH);
-  delay(tiempo);
+  delay(dtled);
   digitalWrite(LED3,LOW);
-  delay(tiempo);
+  delay(dtled);
+  }
+  else {  
+  digitalWrite(LED3,HIGH);
+  delay(dtled);
+  digitalWrite(LED3,LOW);
+  delay(dtled);
+  digitalWrite(LED2,HIGH);
+  delay(dtled);
+  digitalWrite(LED2,LOW);
+  delay(dtled);
+  digitalWrite(LED1,HIGH);
+  delay(dtled);
+  digitalWrite(LED1,LOW);
+  delay(dtled);
+    }
+
 }
---
+++ sketch_mar2a/sketch_mar2a.ino
@@ -0,0 +1,35 @@
+//EJERCICIO 2 
+
+#define LED1 1 
+#define PULS 8
+#define BAUDRATE 9600
+int valpul;
+int dtled=200;
+volatile int cont =0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN ,OUTPUT);
+  pinMode(PULS,INPUT_PULLUP);
+  attachInterrup (digitalPinToInterrupt(PULS),counter,RISING);
+  
+  Serial.begin(BAUDRATE);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+valpul=digitalRead(PULS);
+Serial.println(100);
+Serial.print("CONTADOR:     ");
+Serial.println(cont);
+
+
+digitalWrite(LED1,HIGH);
+  delay(100);
+  digitalWrite(LED1,LOW);
+ 
+ 
+
+
+
+}
